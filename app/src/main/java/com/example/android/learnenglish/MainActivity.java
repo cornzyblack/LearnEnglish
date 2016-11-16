fb084c3443a3ad64ce38eb90975a422b7c8d6f4f
weheardwire <eniwokepdm@gmail.com>
2016-11-16 14:50:50 +0100

    Creating the Learn English Nupe App

--- app/src/main/java/com/example/android/learnenglish/MainActivity.java
+++ app/src/main/java/com/example/android/learnenglish/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.learnenglish;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        //Set a click listener on that view
+        numbers.setOnClickListener(new View.OnClickListener() {
+            //The code in this method will be executed when the numbers category is clicked on
+            @Override
+            public void onClick(View view) {
+                //Create a new Intent to open the {@link NumbersActivity
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
     }
 }
